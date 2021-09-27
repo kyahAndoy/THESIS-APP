c9c752b6ad33c192031b989c643d88806e9f0d13
Zandro Gene Ortecio <kyahandoy@gmail.com>
2021-09-27 23:43:39 +0800

    adding pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router, Switch, Route
+} from "react-router-dom";
+
+
+import Home from './Home';
+import Videos from './Videos';
+import Images from './Images';
+import Downloads from './Downloads';
+import Container from "@material-ui/core/Container";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-
+    <>
+        <Router>
+        <Container maxWidth="xs">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/videos" component={Videos} />
+              <Route path="/images" component={Images} />
+              <Route path="/downloads" component={Downloads} />
+        </Switch>
+      
+        </Container>
+         </Router>
+    </>
 
   );
 }
---
+++ src/Images.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function Images(){
+    return (
+        <div>
+            <h1>Image Page</h1>
+        </div>
+    )
+}
