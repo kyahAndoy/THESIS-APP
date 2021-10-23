512955e7a99065328a6317d1fe7585ecc015b08f
Zandro Gene Ortecio <kyahandoy@gmail.com>
2021-10-23 23:21:28 +0800

    sdasd

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import ButtonHolder from './components/ButtonHolder';
-import useStyles from './components/componentStyles/ComponentsStyle';
 import Player from './components/Player';
-import { borders } from '@mui/system';
+
 
 function Home() {
-  const classes = useStyles();
+
   return (
     <div>
       <h1>Home Page</h1>
