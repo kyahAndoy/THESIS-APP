4964f20e9a17c68cbb495ad5b39480963be3328c
Zandro Gene Ortecio <kyahandoy@gmail.com>
2021-10-23 23:24:15 +0800

    fsasdf

--- src/components/ButtonHolder.js
+++ src/components/ButtonHolder.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Paper from '@mui/material/Paper';
 import useStyles from './componentStyles/ComponentsStyle';
-import { borders } from '@mui/system';
+
 
 
 const ButtonHolder = () => {
